254052ed2c8ec50b028ef86ef9c07e971060f1fc
Heo Jong Tae <heo9910@naver.com>
2022-01-19 18:01:37 +0900

    .

--- chapter-02-column-mapping/src/main/java/com/jpa/study/Member.java
+++ chapter-02-column-mapping/src/main/java/com/jpa/study/Member.java
@@ -23,13 +23,14 @@ public class Member {
     private Long id;
     
     /* *
-     * insertable updatable JPA 상에서 데이터 삽입과 변경 가능여부 , 
-     * nullable null 허용여부 (DDL, DML 반영)
+     * insertable, updatable : JPA 상에서 데이터 삽입과 변경 가능여부 , 
+     * nullable : null 허용여부 (DDL, DML(하이버네이트) 반영)
+     * length : 문자 길이 제약조건 (DDL)
      * unique 제약 조건 걸기 -> 잘 쓰지 않음 하이버네이트가 제약조건 명을 랜덤으로 생상하기 때문
      * 일반적으로 클래스에 설정하는 @Table을 활용헤서 unique 제약조건 설정
      * ex) @Table(uniqueConstraints = {@UniqueConstraint(name = "NAME_UNIQUE", 
      *                 columnNames = {"NAME, AGE"})}) 
-     * columnDefinition 디비 컬럼 설정 정보 직접 설정(특정 디비에 종속적인 옵션)
+     * columnDefinition 디비 컬럼 설정 정보 직접 설정(특정 디비에 종속적인 옵션, DDL) 
      * ex) columnDefinition = "varchar(100) default 'EMPTY'"
      * */
     @Column(name = "NAME", insertable = true, updatable = true, nullable = false)
@@ -37,8 +38,8 @@ public class Member {
  
     /**
      * BigDecimal과 같은 큰 숫자타입의 경우에
-     * precision : 소수점 포함 전체 숫자 길이 설정
-     * scale : 소수의 자리수 설정
+     * precision : 소수점 포함 전체 숫자 길이 설정 (DDL)
+     * scale : 소수의 자리수 설정 (DDL)
      * 다만 double, float에 적용되지 않는다.
      * 정밀한 소수를 다루어야 할때만 사용
      */
